20bd831ca0fe1d7c5f0407540e737ab035500d31
Richalbert <richalbert@free.fr>
2025-05-01 18:56:18 +0200

    version macOS

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,5 @@
 # File    : __init__.py
 # Date    : 2025-04-26
 # Author  : Richalbert
-# Version : 1.1.0
-# Description : Fichier vide indique que ce dossier est un module Python
+# Version : 1.0.0
+# Description : Fichier vide indique que ce dossier sera un module Python
