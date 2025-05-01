20467ed45fe0918b3071dd8f81c26c89cbff7af0
Richalbert <richalbert@free.fr>
2025-05-01 18:59:53 +0200

    Version macOS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # Project : sort-photos-by-date
-# File    : __init__.py
+# File    : app/__init__.py
 # Date    : 2025-04-26
 # Author  : Richalbert
 # Version : 1.0.0
-# Description : Fichier vide indique que ce dossier sera un module Python
+# Description : Fichier vide indique que le dossier app/ sera un module Python
